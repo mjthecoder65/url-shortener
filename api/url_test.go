a790f4cd3ba499c8b9d56093c6af24561802776d
Michael <mjthecoder53161@gmail.com>
2025-03-11 23:09:07 +0900

    Add url_test.go

---
+++ api/url_test.go
@@ -0,0 +1,23 @@
+package api
+
+import "testing"
+
+func TestCreateShortURL(t *testing.T) {
+
+}
+
+func TestGetShortUrl(t *testing.T) {
+
+}
+
+func TestUpdateShortUrl(t *testing.T) {
+
+}
+
+func TestDeleteShortUrl(t *testing.T) {
+
+}
+
+func TestGetShorUrlStats(t *testing.T) {
+
+}
