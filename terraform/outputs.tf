d43b44d0eb3fb501665ed321c85578ebe7f25eda
Michael <mjthecoder53161@gmail.com>
2025-03-13 17:50:36 +0900

    feat: define ouputs upon running terraform apply

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,24 @@
+output "vpc_name" {
+  value = google_compute_network.vpc.name
+}
 
+output "redis_host" {
+  value = google_redis_instance.redis.host
+}
+
+output "redis_port" {
+  value = google_redis_instance.redis.port
+}
+
+output "artifact_registry_repo_name" {
+  value = google_artifact_registry_repository.repo.name
+}
+
+output "service_account_email" {
+  value = google_service_account.github_deployer.email
+}
+
+output "service_account_key" {
+  value     = google_service_account_key.github_deployer_key.private_key
+  sensitive = true
+}
