4f54416bb1b51738625646f2a39f63128ef6fec6
Michael <mjthecoder53161@gmail.com>
2025-03-16 12:24:21 +0900

    Add varieties of loggs in the endpoints

--- api/url.go
+++ api/url.go
@@ -3,10 +3,18 @@ package api
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mjthecoder65/url-shortener/db"
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const (
+	ErrInvalidURL        = "invalid url format"
+	ErrShortCodeRequired = "short url code is required"
+	ErrShortCodeNotFound = "short url not found"
 )
 
 type CreateShortURLRequest struct {
@@ -15,8 +23,13 @@ type CreateShortURLRequest struct {
 
 func (server *Server) CreateShortURL(ctx *gin.Context) {
 	var req CreateShortURLRequest
+
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		server.logger.WithFields(logrus.Fields{
+			"request": req,
+			"error":   err.Error(),
+		}).Error(ErrInvalidURL)
+		ctx.JSON(http.StatusBadRequest, errorResponse(ErrInvalidURL))
 		return
 	}
 
@@ -24,28 +37,65 @@ func (server *Server) CreateShortURL(ctx *gin.Context) {
 		URL: req.URL,
 	}
 
-	url, err := server.queries.CreateShortURL(context.Background(), server.config, arg)
+	dbContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	url, err := server.queries.CreateShortURL(dbContext, server.config, arg)
 
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		server.logger.WithFields(logrus.Fields{
+			"url":   req.URL,
+			"error": err.Error(),
+		}).Error("Failed to create short URL")
+		ctx.JSON(http.StatusInternalServerError, errorResponse("failed to create short url"))
 		return
 	}
+
+	server.logger.WithFields(logrus.Fields{
+		"url":      req.URL,
+		"shortURL": url.ShortCode,
+	}).Info("Short URL Created successfully")
 
 	ctx.JSON(http.StatusCreated, url)
 }
 
 func (server *Server) GetOriginalURL(ctx *gin.Context) {
 	shortCode := ctx.Param("shortCode")
-	url, err := server.queries.GetShortURL(context.Background(), shortCode)
+
+	if shortCode == "" {
+		server.logger.WithFields(logrus.Fields{
+			"shortCode": shortCode,
+		}).Warn(ErrShortCodeRequired)
+		ctx.JSON(http.StatusBadRequest, errorResponse(ErrShortCodeRequired))
+		return
+	}
+
+	dbContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	url, err := server.queries.GetShortURL(dbContext, shortCode)
 
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			server.logger.WithFields(logrus.Fields{
+				"shortCode": shortCode,
+			}).Warn(ErrShortCodeNotFound)
+			ctx.JSON(http.StatusNotFound, errorResponse(ErrShortCodeNotFound))
 			return
 		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+
+		server.logger.WithFields(logrus.Fields{
+			"shortCode": shortCode,
+			"error":     err.Error(),
+		}).Error("Failed to fetch short URL")
+		ctx.JSON(http.StatusInternalServerError, errorResponse("failed to fetch short url"))
 		return
 	}
+
+	server.logger.WithFields(logrus.Fields{
+		"shortCode": shortCode,
+		"url":       url.URL,
+	}).Info("Short URL retried successfully")
 
 	ctx.JSON(http.StatusOK, url)
 }
@@ -56,27 +106,56 @@ type UpdateShortURLRequest struct {
 
 func (server *Server) UpdateShortURL(ctx *gin.Context) {
 	var req UpdateShortURLRequest
+
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		server.logger.WithFields(logrus.Fields{
+			"request": req,
+			"error":   err.Error(),
+		}).Error(ErrInvalidURL)
+		ctx.JSON(http.StatusBadRequest, errorResponse(ErrInvalidURL))
 		return
 	}
+
 	shortCode := ctx.Param("shortCode")
+
+	if shortCode == "" {
+		server.logger.WithFields(logrus.Fields{
+			"shortCode": shortCode,
+		}).Warn(ErrShortCodeRequired)
+		ctx.JSON(http.StatusBadRequest, errorResponse(ErrShortCodeRequired))
+		return
+	}
 
 	arg := db.UpdateShortURLParams{
 		ShortCode: shortCode,
 		URL:       req.URL,
 	}
 
-	url, err := server.queries.UpdateShortURL(context.Background(), arg)
+	dbContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	url, err := server.queries.UpdateShortURL(dbContext, arg)
 
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			server.logger.WithFields(logrus.Fields{
+				"shortCode": shortCode,
+			}).Warn(ErrShortCodeNotFound)
+			ctx.JSON(http.StatusNotFound, errorResponse(ErrShortCodeNotFound))
 			return
 		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		server.logger.WithFields(logrus.Fields{
+			"shortCode": shortCode,
+			"error":     err.Error(),
+		}).Error("Failed to update short URL")
+		ctx.JSON(http.StatusInternalServerError, errorResponse("failed to update short url"))
 		return
 	}
+
+	server.logger.WithFields(logrus.Fields{
+		"shortCode": shortCode,
+		"url":       req.URL,
+	}).Info("Short URL updated successfully")
 
 	ctx.JSON(http.StatusOK, url)
 }
@@ -84,34 +163,80 @@ func (server *Server) UpdateShortURL(ctx *gin.Context) {
 func (server *Server) DeleteShortURL(ctx *gin.Context) {
 	shortCode := ctx.Param("shortCode")
 
-	err := server.queries.DeleteShortURL(context.Background(), shortCode)
+	if shortCode == "" {
+		server.logger.WithFields(logrus.Fields{
+			"shortCode": shortCode,
+		}).Warn(ErrShortCodeRequired)
+		ctx.JSON(http.StatusBadRequest, errorResponse(ErrShortCodeRequired))
+		return
+	}
+
+	dbContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.queries.DeleteShortURL(dbContext, shortCode)
 
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			server.logger.WithFields(logrus.Fields{
+				"shortCode": shortCode,
+			}).Warn(ErrShortCodeNotFound)
+			ctx.JSON(http.StatusNotFound, errorResponse(ErrShortCodeNotFound))
 			return
 		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		server.logger.WithFields(logrus.Fields{
+			"shortCode": shortCode,
+			"error":     err.Error(),
+		}).Error("Failed to delete short URL")
+		ctx.JSON(http.StatusInternalServerError, errorResponse("failed to delete short url"))
 		return
 	}
-	ctx.JSON(http.StatusNoContent, nil)
+
+	server.logger.WithFields(logrus.Fields{
+		"shortCode": shortCode,
+	}).Info("Short URL delete successfully")
+
+	ctx.Status(http.StatusNoContent)
 }
 
 func (server *Server) GetURLStats(ctx *gin.Context) {
 	shortCode := ctx.Param("shortCode")
 
-	url, err := server.queries.GetShortURLStats(context.Background(), shortCode)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return
-		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	if shortCode == "" {
+		server.logger.WithFields(logrus.Fields{
+			"shortCode": shortCode,
+		}).Warn(ErrShortCodeRequired)
+		ctx.JSON(http.StatusBadRequest, errorResponse(ErrShortCodeRequired))
 		return
 	}
+
+	dbContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	url, err := server.queries.GetShortURLStats(dbContext, shortCode)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			server.logger.WithFields(logrus.Fields{
+				"shortCode": shortCode,
+			}).Warn(ErrShortCodeNotFound)
+			ctx.JSON(http.StatusNotFound, errorResponse(ErrShortCodeNotFound))
+			return
+		}
+		server.logger.WithFields(logrus.Fields{
+			"shortCode": shortCode,
+			"error":     err.Error(),
+		}).Error("Failed to Fetch short URL")
+		ctx.JSON(http.StatusInternalServerError, errorResponse("failed to fetch url"))
+		return
+	}
+
+	server.logger.WithFields(logrus.Fields{
+		"shortCode": shortCode,
+	}).Info("Short URL stats retrieved succefully")
+
 	ctx.JSON(http.StatusOK, url)
 }
 
-func errorResponse(err error) gin.H {
-	return gin.H{"error": err}
+func errorResponse(message string) gin.H {
+	return gin.H{"error": message}
 }
